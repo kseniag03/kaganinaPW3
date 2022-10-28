cb550c67a369a271596ca049a1581085a38e8a84
kseniag03 <kaganina@edu.hse.ru>
2022-10-28 17:12:43 +0300

    Initial Commit

---
+++ kaganinaPW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kaganinaPW3
+//
+//  Created by Юлия Бакурская on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
